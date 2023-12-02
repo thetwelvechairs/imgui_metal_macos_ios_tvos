4fb9b50dc0dbc9a90ddd0089d2197df8b49b0ad2
vokamis <asimakov@twelvechairs-software.com>
2023-12-02 10:51:36 -0500

    vision

---
+++ visionOS/ShaderTypes.h
@@ -0,0 +1,13 @@
+//
+//  ShaderTypes.h
+//  visionOS
+//
+//  Created by air on 11/26/23.
+//  Copyright © 2023 Warren Moore. All rights reserved.
+//
+
+#ifndef ShaderTypes_h
+#define ShaderTypes_h
+
+
+#endif /* ShaderTypes_h */
---
+++ visionOS/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  visionOS
+//
+//  Created by air on 11/26/23.
+//  Copyright © 2023 Warren Moore. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
